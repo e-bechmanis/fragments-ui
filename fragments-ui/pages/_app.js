bd079c6ec61fdadb7d1e1ec28cfb96d1394a0147
Elena Bechmanis <ebechmanis@myseneca.ca>
2023-02-12 23:51:36 -0500

    added ui or adding fragment

--- fragments-ui/pages/_app.js
+++ fragments-ui/pages/_app.js
@@ -1,7 +1,9 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import { getUserFragments } from "./api/api";
 import { Auth, getUser } from "../lib/auth";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { postUserFragment } from "./api/api";
 import "@aws-amplify/ui-react/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,6 +12,20 @@ export default function App() {
     getUser().then((user) => {
       console.log(user);
       getUserFragments(user);
+    });
+  }
+
+  const [fragment, setFragment] = useState("");
+  const [warning, setWarning] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    getUser().then((user) => {
+      try {
+        postUserFragment(user, fragment);
+      } catch (err) {
+        setWarning(err.message);
+      }
     });
   }
 
@@ -26,7 +42,37 @@ export default function App() {
             }}
             onLoad={connectToApi()}
           >
-            <Card.Title>Hello {user.username}</Card.Title>
+            <Card.Title>
+              Hello {user.username}
+              <br />
+              <br />
+            </Card.Title>
+
+            <Form onSubmit={handleSubmit}>
+              <Form.Group>
+                <Form.Label>Fragment</Form.Label>
+                <br />
+                <Form.Control
+                  type="text"
+                  id="text-fragment"
+                  name="text-fragment"
+                  onChange={(e) => setFragment(e.target.value)}
+                />
+              </Form.Group>
+
+              {warning && (
+                <>
+                  <br />
+                  <Alert variant="danger">{warning}</Alert>
+                </>
+              )}
+
+              <br />
+              <Button variant="outline-secondary" type="submit">
+                Add
+              </Button>
+            </Form>
+
             <Button
               style={{ marginTop: "2rem" }}
               variant="warning"
--- fragments-ui/pages/api/api.js
+++ fragments-ui/pages/api/api.js
@@ -26,3 +26,43 @@ export async function getUserFragments(user) {
     console.error("Unable to call GET /v1/fragments", { err });
   }
 }
+
+export async function postUserFragment(user) {
+  console.log("Sending user fragments data...");
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: "POST",
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(),
+      "Content-Type": "text/plain",
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    if (res.status === 200) {
+      console.log("Success", { data });
+      return data;
+    }
+  } catch (err) {
+    console.error("Unable to call POST /v1/fragments", { err });
+  }
+}
+
+export async function getFragmentById(id) {
+  console.log("Requesting user fragment data");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      // Generate headers with the proper Authorization bearer token to pass
+      headers: user.authorizationHeaders(),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log("Got user fragment data", { data });
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragments", { err });
+  }
+}
